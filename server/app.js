c8d969a469f2ed3ae662d77cd97e97a2feae8af4
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-23 12:28:45 +0200

    public

--- server/app.js
+++ server/app.js
@@ -6,8 +6,10 @@ const accountRouter = require("./routes/accountRoutes");
 const app = express();
 const path = require("path");
 
-const publicPath = path.join(__dirname, "client/build");
-app.use(express.static(publicPath));
+// const publicPath = path.join(__dirname, "client/build");
+// app.use(express.static(publicPath));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use(express.json());
@@ -16,8 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/bank", bankRouter);
 app.use("/accounts", accountRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(publicPath);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 module.exports = app;
